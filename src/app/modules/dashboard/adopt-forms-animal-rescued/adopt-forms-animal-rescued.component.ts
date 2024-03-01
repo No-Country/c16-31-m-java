1debb50ef654ce85fc87e7a371d07dc57457bdd5
Sofii283soto <sofiasoto283@gmail.com>
2024-03-01 11:29:19 -0300

    ADD: component for forms animals rescued

---
+++ src/app/modules/dashboard/adopt-forms-animal-rescued/adopt-forms-animal-rescued.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adopt-forms-animal-rescued',
+  templateUrl: './adopt-forms-animal-rescued.component.html',
+  styleUrls: ['./adopt-forms-animal-rescued.component.css']
+})
+export class AdoptFormsAnimalRescuedComponent {
+
+}
--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -7,6 +7,7 @@ import { AnimalProfileComponent } from './animal-profile/animal-profile.componen
 const routes: Routes = [
   { path: '', component: AdoptComponent },
   { path: 'adopts-forms', component: AdoptFormsComponent },
+  { path: 'adopts-forms-animalRescued', component: AdoptFormsComponent },
   { path: 'aaa', component: AnimalProfileComponent }
 ];
 
